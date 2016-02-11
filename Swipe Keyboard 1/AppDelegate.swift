c7cfa49f614a641de5dd5cb9b6d9967273332e1e
Vishal Agarwal <agarwalvishal1994@gmail.com>
2016-02-11 17:31:23 +0530

    Initial Commit

---
+++ Swipe Keyboard 1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Swipe Keyboard 1
+//
+//  Created by Vishal Agarwal on 11/02/16.
+//  Copyright © 2016 Vishal Agarwal. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Swipe Keyboard 1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Swipe Keyboard 1
+//
+//  Created by Vishal Agarwal on 11/02/16.
+//  Copyright © 2016 Vishal Agarwal. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
